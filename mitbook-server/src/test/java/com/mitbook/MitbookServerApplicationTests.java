ab83608aa580402af730d9261519f3c370aa3755
mitbook <13656694002@163.com>
2020-07-01 13:48:21 +0800

    Add test class

--- mitbook-server/src/test/java/com/mitbook/MitbookServerApplicationTests.java
+++ mitbook-server/src/test/java/com/mitbook/MitbookServerApplicationTests.java
@@ -3,6 +3,9 @@ package com.mitbook;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ * @author pengzhengfa
+ */
 @SpringBootTest
 class MitbookServerApplicationTests {
 
