1d17bbc81131cbfc837b2f600c0f39a7fc4985cb
pengzhengfa <13656694002@163.com>
2020-07-08 13:59:05 +0800

    Add client test code

---
+++ mitbook-client/src/test/java/com/mitbook/MitbookClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mitbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MitbookClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
