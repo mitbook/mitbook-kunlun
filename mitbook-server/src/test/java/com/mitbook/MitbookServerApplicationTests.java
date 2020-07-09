bfe682c2a99e2999190458255f41bf522fe5f7aa
pengzhengfa <13656694002@163.com>
2020-07-09 10:27:18 +0800

    Add startup class

---
+++ mitbook-server/src/test/java/com/mitbook/MitbookServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mitbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MitbookServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
